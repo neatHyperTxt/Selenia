fb7589519307126c0846579786570dcd334b1335
Vidit Gupta <viditgupta700@gmail.com>
2024-10-24 23:38:23 +0530

    Visual Changes in Register Page

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -91,38 +91,36 @@ function Register() {
   return (
     <div className={styles.container}>
       <div>
-        <img src="https://plus.unsplash.com/premium_photo-1706625669518-664f071e02af?w=700&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGV4cGxvcmV8ZW58MHx8MHx8fDA%3D" alt="" />
+        <div className={styles.imageContainer}>
+        <img src="https://plus.unsplash.com/premium_photo-1669741909456-61b8b9b3f329?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8dmVydGljYWwlMjBsYW5kc2NhcGV8ZW58MHwxfDB8fHww" alt="" />
+        </div>
       </div>
       <form className={styles.form} onSubmit={formHandler} action="">
         <h1>Create an account</h1>
+        <p>Already an account? <a href="">Log in</a></p>
         <div className={styles.fullnameContainer}>
           <div className={styles.fname}>
-            <label htmlFor="fname">First Name</label>
             <Input type="text" name="fname" id="fname" value={firstName} onChange={firstNameHandler} placeholder="First Name"/>
             <p className={styles.paraErrorWarnings}>{formErrors.fname}</p>
           </div>
           <div className={styles.lname}>
-            <label htmlFor="lname">Last Name</label>
             <Input type="text" name="lname" id="lname" value={lastName} onChange={lastNameHandler} placeholder="Last Name"/>
             <p className={styles.paraErrorWarnings}>{formErrors.lname}</p>
           </div>
         </div>
         <div>
-          <label htmlFor="uname">Username</label>
-          <Input type="text" name="uname" id="uname" value={username} onChange={usernameHandler} placeholder="User Name"/>
-          <p className={styles.paraErrorWarnings}>{formErrors.uname}</p>
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
           <Input type="email" name="email" id="email" value={email} onChange={emailHandler} placeholder="Email"/>
           <p className={styles.paraErrorWarnings}>{formErrors.email}</p>
         </div>
         <div>
-          <label htmlFor="password">Password</label>
-          <Input type="password" name="password" id="password" value={password} onChange={passwordHandler} placeholder="Password"/>
+          <Input type="password" name="password" id="password" value={password} onChange={passwordHandler} placeholder="Enter your password"/>
           <p className={styles.paraErrorWarnings}>{formErrors.password}</p>
         </div>
-        <Button type="submit" title="Register"/>
+        <label className={styles.terms_label}>
+        <input type="checkbox" name="terms" required/>
+            I agree to the <a href=""> Terms & Conditions</a>
+          </label>
+        <Button type="submit" title="Create Account"/>
       </form>
       <div/>
     </div>
